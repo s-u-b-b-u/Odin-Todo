97bf7becd26275c95d1cb871813103a5f45c70ac
Subbu <rsubbu931@gmail.com>
2026-07-24 20:55:43 +0530

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+import path from "node:path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+  ],
+}
